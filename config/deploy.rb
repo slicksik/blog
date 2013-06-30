9f397a094159b656520a9fa562d099acc6fbfc57
Vasileios Topouz <vtopouz@cyclopsweb.gr>
2013-07-01 00:22:03 +0300

    switched to sqlite

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application, "blog"
 set :user, "root"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
-set :use_sudo, false
+set :use_sudo, true
 
 set :scm, "git"
 set :repository, "git@github.com:slicksik/#{application}.git"
